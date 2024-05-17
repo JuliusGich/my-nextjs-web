c3ce3802efee916185684f86f785287fceffc210
julius <juliusgichure145@gmsil.com>
2024-05-16 23:31:51 -0700

    update 1

---
+++ src/app/navbar/page.tsx
@@ -0,0 +1,17 @@
+export default function Navbar(){
+    return(
+        <nav className="navbar">
+            <div className="logo">
+           <p>Welcome</p>
+        </div>
+            <div className="nav-links">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+            </div>
+        </nav>
+    )
+}
